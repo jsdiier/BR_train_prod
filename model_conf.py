b546083abc905c0474a0c6969b40c31ed61fae05
didi <didi@MacBook-Pro-113.local>
2026-08-18 11:44:59 +0800

    baseline: batch_size 1024->512, learning_rate 0.0003->0.001

--- model_conf.py
+++ model_conf.py
@@ -5,8 +5,8 @@ gpu_id = '0,'
 done_file_path = 'model/model.done'
 
 epoch_num = 1
-batch_size = 1024
-learning_rate = 0.0003
+batch_size = 512
+learning_rate = 0.001
 l2_reg = 0.0001
 use_bn = True
 
