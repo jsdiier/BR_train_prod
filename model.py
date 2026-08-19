ecc0393b6808144b1384ffa8ec7d2519e13a6c40
didi <didi@MacBook-Pro-113.local>
2026-08-19 16:10:06 +0800

    mmoe_task_gate: per-task MMOE gating on shared representation

--- model.py
+++ model.py
@@ -8,6 +8,7 @@ from tensorflow.python.framework import sparse_tensor
 from module.rankmixer_v4 import *
 from logger import logger
 from module.seq_attention import *
+from module.moe import MMOE
 
 
 class Model(tf.keras.Model):
@@ -104,6 +105,9 @@ class Model(tf.keras.Model):
         # 初始化底层主网络
         self.rankmixer = RankMixer(t=16, token_dim=768, num_heads=16, num_experts=16, hidden_ratio=2,
                                    training=self.training)
+        self.mmoe = MMOE(num_experts=4, num_tasks=4, expert_dims=[512, 256, 128],
+                         gate_dims=[128], use_bn=self.use_bn, l2_reg=model_conf.l2_reg,
+                         training=self.training)
         # 初始化序列网络
         self.seq_click_attention_layer = DIN_attention_Layer([50, 20], 'sigmoid', name='global_click_seq')
         self.seq_pay_attention_layer = DIN_attention_Layer([50, 20], 'sigmoid', name='global_pay_seq')
@@ -577,10 +581,13 @@ class Model(tf.keras.Model):
 
         concat = tf.concat([lr, fm, rankmixer_output], axis=1)
 
-        buy_tower_output = self.buy_tower(concat, training=self.training)
-        cat_tower_output = self.cat_tower(concat, training=self.training)
-        click_tower_output = self.click_tower(concat, training=self.training)
-        ext_tower_output = self.ext_tower(concat, training=self.training)
+        task_inputs = [concat, concat, concat, concat]
+        task_outputs = self.mmoe(concat, task_inputs, lr, fm)
+
+        buy_tower_output = self.buy_tower(task_outputs[0], training=self.training)
+        cat_tower_output = self.cat_tower(task_outputs[1], training=self.training)
+        click_tower_output = self.click_tower(task_outputs[2], training=self.training)
+        ext_tower_output = self.ext_tower(task_outputs[3], training=self.training)
 
         cvr_pred_org = self.dense_concat(buy_tower_output)
         cat_pred_org = self.dense_concat1(cat_tower_output)
@@ -600,4 +607,3 @@ class Model(tf.keras.Model):
             return final_pred, cvr_score, ctr_score, cat_score, ext_score
 
         return ctcvr, cat_pred, click_pred, ext_pred
-
