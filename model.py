3c26700bf2804d177311aba12ae9271abef87c69
didi <didi@MacBook-Pro-113.local>
2026-08-20 10:38:56 +0800

    seq_attention_multihead: multi-head DIN attention

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ import model_conf
 from tensorflow.python.framework import sparse_tensor
 from module.rankmixer_v4 import *
 from logger import logger
-from module.seq_attention import *
+from module.seq_attention import DIN_attention_Layer, MultiHeadDINAttention
 
 
 class Model(tf.keras.Model):
@@ -105,13 +105,13 @@ class Model(tf.keras.Model):
         self.rankmixer = RankMixer(t=16, token_dim=768, num_heads=16, num_experts=16, hidden_ratio=2,
                                    training=self.training)
         # 初始化序列网络
-        self.seq_click_attention_layer = DIN_attention_Layer([50, 20], 'sigmoid', name='global_click_seq')
-        self.seq_pay_attention_layer = DIN_attention_Layer([50, 20], 'sigmoid', name='global_pay_seq')
-        self.seq_12h_click_cate_id_attention_layer = DIN_attention_Layer([50, 20], 'sigmoid',
-                                                                         name='12h_click_cate_id_seq')
-        self.attention_layer_search_long_pay = DIN_attention_Layer([50, 20], 'sigmoid', name='search_pay_seq_long')
-        self.attention_layer_search_long_clk = DIN_attention_Layer([50, 20], 'sigmoid', name='search_clk_seq_long')
-        self.attention_layer_search_long_query = DIN_attention_Layer([50, 20], 'sigmoid', name='search_query_seq_long')
+        self.seq_click_attention_layer = MultiHeadDINAttention([50, 20], 'sigmoid', name='global_click_seq')
+        self.seq_pay_attention_layer = MultiHeadDINAttention([50, 20], 'sigmoid', name='global_pay_seq')
+        self.seq_12h_click_cate_id_attention_layer = MultiHeadDINAttention([50, 20], 'sigmoid',
+                                                                           name='12h_click_cate_id_seq')
+        self.attention_layer_search_long_pay = MultiHeadDINAttention([50, 20], 'sigmoid', name='search_pay_seq_long')
+        self.attention_layer_search_long_clk = MultiHeadDINAttention([50, 20], 'sigmoid', name='search_clk_seq_long')
+        self.attention_layer_search_long_query = MultiHeadDINAttention([50, 20], 'sigmoid', name='search_query_seq_long')
 
         # 搜索长序列：先融合多路 embedding，再与 DIN 注意力 + 均值池化残差组合，减轻「高维 concat 噪声」
         seq_token_dim = 32
@@ -600,4 +600,3 @@ class Model(tf.keras.Model):
             return final_pred, cvr_score, ctr_score, cat_score, ext_score
 
         return ctcvr, cat_pred, click_pred, ext_pred
-
--- module/seq_attention.py
+++ module/seq_attention.py
@@ -37,3 +37,40 @@ class DIN_attention_Layer(tf.keras.layers.Layer):
         outputs = tf.squeeze(tf.matmul(outputs, v), axis=1)
 
         return outputs
+
+class MultiHeadDINAttention(tf.keras.layers.Layer):
+    def __init__(self, att_hidden_units, activation='relu', num_heads=4, head_dim=8, name=''):
+        super(MultiHeadDINAttention, self).__init__()
+        self.num_heads = num_heads
+        self.head_dim = head_dim
+        self.dim = num_heads * head_dim
+        self.q_dense = tf.keras.layers.Dense(self.dim, name=f'{name}_mh_q_dense')
+        self.k_dense = tf.keras.layers.Dense(self.dim, name=f'{name}_mh_k_dense')
+        self.v_dense = tf.keras.layers.Dense(self.dim, name=f'{name}_mh_v_dense')
+
+    def call(self, inputs):
+        q, k, v, mask = inputs
+        batch_size = tf.shape(q)[0]
+        seq_len = tf.shape(k)[1]
+
+        q = tf.expand_dims(q, axis=1)
+        q = self.q_dense(q)
+        k = self.k_dense(k)
+        v = self.v_dense(v)
+
+        def split_heads(x):
+            x = tf.reshape(x, [batch_size, seq_len, self.num_heads, self.head_dim])
+            return tf.transpose(x, [0, 2, 1, 3])
+
+        qh = split_heads(q)
+        kh = split_heads(k)
+        vh = split_heads(v)
+
+        scores = tf.matmul(qh, kh, transpose_b=True) / tf.sqrt(tf.cast(self.head_dim, tf.float32))
+        mask_expanded = tf.expand_dims(tf.expand_dims(mask, axis=1), axis=1)
+        scores = tf.where(tf.equal(mask_expanded, 0), -1e9, scores)
+        att = tf.nn.softmax(scores, axis=-1)
+
+        out = tf.reduce_sum(att * vh, axis=-2)
+        out = tf.reshape(out, [batch_size, self.num_heads * self.head_dim])
+        return out
